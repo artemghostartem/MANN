5435893f58c443b2ac6359434d87f74768d5f325
artemghostartem <artemghostartem@gmail.com>
2020-11-05 23:45:29 +0200

    no message

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,23 @@
 $(function() {
+	$( document ).ready(function() {
+		let heroTegline = document.querySelector('.hero-main-tagline')
+		let heroTeglineWidth = heroTegline.offsetWidth
+		let heroTeglineText = document.querySelector('.hero-main-tagline p')
+		let heroTeglineTextHeight = heroTeglineText.offsetHeight
+		
+		function setTeglineMargin () {
+			heroTeglineText.style = `margin-left: -${heroTeglineWidth/2-heroTeglineTextHeight/2}px`
+		}
 
+		setTeglineMargin()
 
+		let heroSection = document.querySelector('.hero-section')
+		let heroSectionHeight = heroSection.offsetHeight
 
+		function setTeglineTop () {
+			heroTegline.style = `top: ${heroSectionHeight/2}px`
+		}
+
+		setTeglineTop ()
+	}) 
 });
