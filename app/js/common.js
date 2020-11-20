e6b5ca5314c42d7f634976962ed19d2431cc78ab
artemghostartem <artemghostartem@gmail.com>
2020-11-20 14:10:39 +0200

    desctop fix

--- app/js/common.js
+++ app/js/common.js
@@ -49,4 +49,54 @@ $(document).ready(function() {
     		$('.logo').toggleClass('active-menu');
     	}    	
     })
+    // Zoom-gallery
+    $('.zoom-gallery').magnificPopup({
+		delegate: 'a',
+		type: 'image',
+		closeOnContentClick: false,
+		closeBtnInside: false,
+		mainClass: 'mfp-with-zoom mfp-img-mobile',
+		image: {
+			verticalFit: true,
+			titleSrc: function(item) {
+				return item.el.attr('title') + ' &middot; <a class="image-source-link" href="'+item.el.attr('data-source')+'" target="_blank">image source</a>';
+			}
+		},
+		gallery: {
+			enabled: true
+		},
+		zoom: {
+			enabled: true,
+			duration: 300, // don't foget to change the duration also in CSS
+			opener: function(element) {
+				return element.find('img');
+			}
+		}
+		
+	});	
+    let mainSlider = $('.first-carousel');
+	mainSlider.owlCarousel({
+		items:1,
+		loop: true,
+		margin:0,
+		nav:true,
+        mouseDrag: false,
+        touchDrag: false,
+
+	});
+    let secondSlider = $('.second-carousel');
+    secondSlider.owlCarousel({
+        items:3,
+        loop: true,
+        margin:0,
+        nav:true,
+        mouseDrag: false,
+        touchDrag: false,
+
+    });
+
+    $('.carousel-next').click(function() {
+        mainSlider.trigger('next.owl.carousel');
+        secondSlider.trigger('next.owl.carousel');
+    })
 })
